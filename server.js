254ceae9abef97b405d9bc0a7766bb73ed3ac4a9
michgrenda <f.m.grenda@gmail.com>
2020-09-28 11:03:36 +0200

    Add unsaved deployment changes

--- server.js
+++ server.js
@@ -18,6 +18,16 @@ app.use("/api/columns", require("./routes/api/columns"));
 app.use("/api/tasks", require("./routes/api/tasks"));
 app.use("/api/auth", require("./routes/api/auth"));
 
+// Serve static assests in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
